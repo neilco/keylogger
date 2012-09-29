9da8b94ed409a5f6c38c2e76de42afffb7dc71d1
Neil Cowburn <neilco@gmail.com>
2012-09-29 12:18:03 +0100

    Initial Commit

---
+++ keylogger/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  keylogger
+//
+//  Created by Neil on 29/09/2012.
+//  Copyright (c) 2012 Neil Cowburn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
